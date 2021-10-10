75e398a3d53eb94b2b57f5b95ff2c573f32d0c08
Marcelo Bogas <marcelobogas@hotmail.com>
2021-10-10 12:06:41 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Pix\Payload;
+use Mpdf\QrCode\QrCode;
+use Mpdf\QrCode\OutPut;
+
+require __DIR__ . '/vendor/autoload.php';
+
+/* instância principal do payload pix */
+$objPayload = (new Payload)->setPixKey('12345678910')
+                           ->setDescription('Pagamento de Mensalidade')
+                           ->setMerchantName('Nome do Pagador')
+                           ->setMerchantCity('Cidade do Pagador')
+                           ->setAmount(0.10)
+                           ->setTxId('1234567');
+
+/* código de pagamento PIX */
+$payloadQrCode = $objPayload->getPayLoad();
+
+/* QR CODE */
+$objQrCode = new QrCode($payloadQrCode);
+
+/* imagem do QR CODE */
+$image = (new OutPut\Png)->output($objQrCode,400);
+
+?>
+
+<h1>QR CODE - PIX</h1>
+<br>
+<img src="data:image/png;base64, <?= base64_encode($image); ?>" alt="qr-code">
+<br><br>
+Código Pix para pagamento:<br>
+<strong><?= $payloadQrCode; ?></strong>
